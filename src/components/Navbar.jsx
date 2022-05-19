b07a548e3ddefe30c4e016a6384b4fdbfe2f6adf
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-19 16:40:22 +0100

    google fonts and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
       </div>
+      {/* menu */}
       <div>
         <ul>
           <li>Home</li>
