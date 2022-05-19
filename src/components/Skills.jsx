41ba6423535033738accf966e7ec1dc1a60c56c8
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-19 17:29:57 +0100

    initial style for skills tech

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import AWS from '../assets/aws.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div>
+              <p className='text-4xl font-bold inline border-b-4 border-lime-600 '>Skills</p>
+              <p className='py-4'>// These are some of the technologies that I've worked with</p>
+          </div>
 
-export default Skills
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                  <p className='my-4'>HTML</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
