60d02a006b263cc0293d3a15d58b3fc86947b01c
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-21 17:56:47 +0100

    add getform end point for form submission

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href= '{Pdf}'
+              href= '{Pdf} target = "_blank" rel="noreferrer'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
