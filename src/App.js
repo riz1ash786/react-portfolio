acf1ce4a7cbeac8eda1d2c5ae8da511a944c9589
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-21 14:50:15 +0100

    setup tailwind and configure app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <div>
